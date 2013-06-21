99ccbb8bbaa9d06fc86e0054a260cac7bf3536ad
alex.donato@gmail.com <alex.donato@gmail.com@73f32bef-280e-31a8-afc3-c039c5b653f7>
2013-06-21 05:25:45 +0000

    aaa

---
+++ trunk/src/com/pippo/TestMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.pippo;
+
+/**
+ * @author Alessandro Donato
+ *
+ * 21/giu/2013
+ */
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/src/com/pippo/test/SecondoTest.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.pippo.test;
+
+/**
+ * @author Alessandro Donato
+ *
+ * 21/giu/2013
+ */
+public class SecondoTest {
+
+}
